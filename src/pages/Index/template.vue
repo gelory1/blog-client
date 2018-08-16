ce115b95822d4d22abcfbb12d188ea7693af9cdd
gelory <917589697@qq.com>
2018-08-16 21:10:26 +0800

    创建路由

---
+++ src/pages/Index/template.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="index">
+    Home首页
+  </div>
+</template>
+
+<script src="./template.js"></script>
+<style src="./template.css"></style>
---
+++ src/pages/My/template.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="my">
+    个人中心
+  </div>
+</template>
+
+<script src="./template.js"></script>
+<style src="./template.css"></style>
